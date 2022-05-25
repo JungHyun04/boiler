620e7275a87d6f1cbbe9023b950ed9a3cb5e69d1
Seochangbin <tjaudqls@gmail.com>
2022-05-25 13:27:58 +0900

    aa

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
 
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://junghyun:qudtls123@bolierplate.dgck0.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect('', {
 
     }).then(() => console.log("MongDB Connected .."))
     .catch(err => console.log(err))
@@ -12,7 +18,20 @@ mongoose.connect('mongodb+srv://junghyun:qudtls123@bolierplate.dgck0.mongodb.net
 
 
 app.get('/', (req, res) => {
-    res.send('안녕!')
+    res.send('안녕! asdasd')
+})
+
+app.post('/register', (req, res) => {
+
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
 
 app.listen(port, () => {
